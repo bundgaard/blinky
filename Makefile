0388e9b9921619a306caff5363a972d094c80669
David Bundgaard <bundgaard@users.noreply.github.com>
2021-07-18 19:07:50 +0200

    added makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+
+
+build:
+	/opt/microchip/xc8/v2.32/bin/xc8 src/main.c --chip=10F200 -m --opt=all -g --outdir=out
--- src/main.c
+++ src/main.c
@@ -4,7 +4,13 @@
 
 #include <xc.h>
 
+#define _XTAL_FREQ 4000000
 
 void main(void) {
-    
+    while(1) {
+        GPIO = 0xf;
+        __delay_ms(250);
+        GPIO = 0;
+        __delay_ms(250);
+    }
 }
